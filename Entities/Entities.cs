610b6805ff4e4013d8ee29355c2a561f55adc55f
Joseph <joseph351619@gmail.com>
2019-02-19 22:15:08 +0800

    加入專案檔案。

---
+++ Entities/Entities.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace MobileHIS
+{
+    public class Entities : DbContext
+    {
+        public Entities() :base("EntitiesContext") { }
+        public DbSet<Car> Cars { get; set; }
+    }
+}
