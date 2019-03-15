9653f140c4bf060909153bbc7317b3133c0146d7
Joseph <sungfuchiu@gmail.com>
2019-03-15 15:00:19 +0800

    Complete ICD10 manipulation

---
+++ MobileHis_2019/Areas/Settings/Controllers/ICD10Controller.cs
@@ -0,0 +1,39 @@
+﻿using BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using X.PagedList;
+using MobileHis_2019;
+
+namespace MobileHis_2019.Areas.Settings.Controllers
+{
+    public class ICD10Controller : MobileHis_2019.Controllers.BaseController
+    {
+        private ICD10BLL icd10BLL;
+        public ICD10Controller()
+        {
+            icd10BLL = new ICD10BLL();
+        }
+        [HttpGet]
+        public ActionResult Index(int? page, string keyword = "", string type = "")
+        {
+            int currentPageIndex = (page ?? 1) - 1;
+            ViewBag.keyword = keyword;
+            ViewBag.type = type;
+            ViewData.Model = icd10BLL.GetList(keyword, type).ToPagedList(currentPageIndex + 1, GlobalVariable.PageSize);
+            return View();
+        }
+        [HttpPost]
+        public int Create(string code, string name, string type)
+        {
+            return icd10BLL.Add(code, name, type);
+        }
+        [HttpPost]
+        public bool Edit(string code, string name, string type)
+        {
+            return icd10BLL.Edit(code, name, type);
+        }
+    }
+}
