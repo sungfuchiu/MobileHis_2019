14ab71e8a632e8efc2695f8274fee0b1182e6e44
Joseph <joseph351619@gmail.com>
2019-03-19 22:43:42 +0800

    Set base view pae

---
+++ MobileHis_2019/Infrastructure/BaseViewPage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MobileHis.Razor
+{
+    public abstract class BaseViewPage : WebViewPage
+    {
+        public bool _Add { get; set; }
+        public bool _Delete { get; set; }
+        public bool _Update { get; set; }
+        public bool _Print { get; set; }
+        //public virtual new CustomPrincipal User
+        //{
+        //    get { return base.User as CustomPrincipal; }
+        //}
+
+
+        protected override void InitializePage()
+        {
+            _Add = ViewBag._Add == null ? false : ViewBag._Add;
+            _Delete = ViewBag._Delete == null ? false : ViewBag._Delete;
+            _Update = ViewBag._Update == null ? false : ViewBag._Update;
+            _Print = ViewBag._Print == null ? false : ViewBag._Print;
+            base.InitializePage();
+        }
+    }
+    public abstract class BaseViewPage<TModel> : WebViewPage<TModel>
+    {
+
+        public bool _Add { get; set; }
+        public bool _Delete { get; set; }
+        public bool _Update { get; set; }
+        public bool _Print { get; set; }
+        //public virtual new CustomPrincipal User
+        //{
+        //    get { return base.User as CustomPrincipal; }
+        //}
+
+        protected override void InitializePage()
+        {
+            _Add = ViewBag._Add == null ? false : ViewBag._Add;
+            _Delete = ViewBag._Delete == null ? false : ViewBag._Delete;
+            _Update = ViewBag._Update == null ? false : ViewBag._Update;
+            _Print = ViewBag._Print == null ? false : ViewBag._Print;
+            base.InitializePage();
+        }
+    }
+}
