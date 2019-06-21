6a63461143ba319bfb5586885d535189a09e90f5
Joseph <sungfuchiu@gmail.com>
2019-06-21 18:21:18 +0800

    Processing on Authorization

---
+++ MobileHis_2019/Filters/AuthorizeFilter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MobileHis_2019.Filters
+{
+    public class AuthorizeFilter : ActionFilterAttribute
+    {
+        public AuthorizeFilter() { }
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+        }
+    }
+}
