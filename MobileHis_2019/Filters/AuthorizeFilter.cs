f3feea6e545279949a7e1a560825bf77165d59bf
Joseph <sungfuchiu@gmail.com>
2019-06-24 18:16:27 +0800

    Processing on authentication tree

--- MobileHis_2019/Filters/AuthorizeFilter.cs
+++ MobileHis_2019/Filters/AuthorizeFilter.cs
@@ -1,17 +1,175 @@
-﻿using System;
+﻿using Common;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 
 namespace MobileHis_2019.Filters
 {
-    public class AuthorizeFilter : ActionFilterAttribute
+    [Flags]
+    public enum AuthType
     {
-        public AuthorizeFilter() { }
-        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        Add = 1,
+        Delete = 2,
+        Update = 4,
+        Read = 8,
+        Print = 16
+    }
+    public class AuthInfo
+    {
+        public List<AuthParents> Parents { get; set; }
+    }
+    public class AuthParents
+    {
+        public string ParentId { get; set; }
+        public string ParentName { get; set; }
+        public string ParentArea { get; set; }
+        public string ResourceKey { get; set; }
+        public string IconClass { get; set; }
+        public List<AuthItems> Items { get; set; }
+    }
+    public class AuthItems
+    {
+        public string Area { get; set; }
+        public string Control { get; set; }
+        public string Action { get; set; }
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public string ResourceKey { get; set; }
+        public bool HasAdd { get; set; }
+        public bool HasUpdate { get; set; }
+        public bool HasDelete { get; set; }
+        public bool HasPrint { get; set; }
+    }
+    public class AuthenticateFilter : ActionFilterAttribute
+    {
+        string _actionName;
+        AuthType _authType;
+        AuthInfo _authInfo;
+        IPrincipal _principal;
+        //AuthItems _authPage;
+        public AuthenticateFilter(AuthType authType, IPrincipal principle, string actionName="")
         {
-            base.OnActionExecuted(filterContext);
+            _actionName = actionName;
+            _authType = authType;
+            _principal = principle;
+
+        }
+        public AuthInfo AuthInfo {
+            get
+            {
+                if(_authInfo != null)
+                {
+                    string userAuth = HttpContext.Current.Session["userAuth"].ToString();
+                    _authInfo = JsonConvert.DeserializeObject<AuthInfo>(userAuth);
+                }
+                return _authInfo;
+            }
+        }
+        private AuthType _print = AuthType.Print;
+        private AuthType _add = AuthType.Add;
+        private AuthType _delete = AuthType.Delete;
+        private AuthType _read = AuthType.Read;
+        private AuthType _update = AuthType.Update;
+        private string areaName;
+        private string controllerName;
+        private string actionName;
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            try
+            {
+                bool byPass = false;
+                if(AuthInfo == null)
+                {
+                    RedirectToLoginPage(filterContext);
+                }
+                else
+                {
+                    _principal = filterContext.HttpContext.User;
+                    areaName = filterContext.RouteData.DataTokens["area"].ToString();
+                    controllerName = filterContext.RouteData.Values["controller"].ToString();
+                    actionName = (_actionName.IsNullOrEmpty()) ? filterContext.RouteData.GetRequiredString("action").ToString() : _actionName;
+
+                    if(AuthInfo.Parents != null)
+                    {
+                        var authPage = AuthInfo.Parents.SelectMany(x => x.Items.Where(y =>
+                                        HasRoute(y)).Select(y => y)).FirstOrDefault();
+                        if (authPage != null)
+                        {
+                            filterContext.Controller.ViewBag.HasAdd = authPage.HasAdd;
+                            filterContext.Controller.ViewBag.HasDelete = authPage.HasDelete;
+                            filterContext.Controller.ViewBag.HasUpdate = authPage.HasUpdate;
+                            filterContext.Controller.ViewBag.HasPrint = authPage.HasPrint;
+                        }
+                        if (PageCan(_add))
+                        {
+                            byPass = AuthInfo.Parents.Any(a => a.Items.Where(b =>
+                                        HasRoute(b) && b.HasAdd)
+                                        .Any()) || byPass;
+                        }
+                        if (PageCan(_delete))
+                        {
+                            byPass = AuthInfo.Parents.Any(a => a.Items.Where(b =>
+                                        HasRoute(b) && b.HasDelete)
+                                        .Any()) || byPass;
+                        }
+                        if (PageCan(_update))
+                        {
+                            byPass = AuthInfo.Parents.Any(a => a.Items.Where(b =>
+                                        HasRoute(b) && b.HasUpdate)
+                                        .Any()) || byPass;
+                        }
+                        if (PageCan(_print))
+                        {
+                            byPass = AuthInfo.Parents.Any(a => a.Items.Where(b =>
+                                        HasRoute(b) && b.HasPrint)
+                                        .Any()) || byPass;
+                        }
+                        if (PageCan(_read))
+                        {
+                            byPass = AuthInfo.Parents.Any(a => a.Items.Where(b =>
+                                        HasRoute(b)
+                                        ).Any()) || byPass;
+                        }
+                        if (!byPass)
+                        {
+                            RedirectToLoginPage(filterContext);
+                        }
+                    }
+                }
+            }catch(Exception ex)
+            {
+                RedirectToLoginPage(filterContext);
+            }
+            base.OnActionExecuting(filterContext);
+        }
+        private bool HasRoute(AuthItems authItem)
+        {
+            return authItem.Area.Equals(areaName, StringComparison.InvariantCultureIgnoreCase) &&
+                   authItem.Control.Equals(controllerName, StringComparison.InvariantCultureIgnoreCase) &&
+                   authItem.Action.Equals(actionName, StringComparison.InvariantCultureIgnoreCase);
+        }
+        private bool PageCan(AuthType authType)
+        {
+            return (_authType & authType) == _authType;
+        }
+        private void RedirectToLoginPage(ActionExecutingContext filterContext)
+        {
+            if (filterContext.HttpContext.Request.IsAjaxRequest())
+            {
+                filterContext.Result = new EmptyResult();
+                filterContext.HttpContext.Response.StatusCode = 401;
+                filterContext.HttpContext.Response.End();
+            }
+            else
+            {
+                filterContext.Result = new HttpStatusCodeResult(401, "Unauthorized");
+                System.Web.Security.FormsAuthentication.RedirectToLoginPage();
+            }
         }
     }
 }
